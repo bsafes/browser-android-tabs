6a2957f219199cc2d6763987695a3643aad3984d
Serg <serg.zhukovsky@gmail.com>
2016-07-27 17:27:24 -0400

    Added HTTPSE support
    Auditors: @diracdeltas
    
    Conflicts:
    	chrome/browser/net/chrome_network_delegate.cc
    
    Conflicts:
    	chrome/browser/net/chrome_network_delegate.cc
    
    Conflicts:
    	chrome/android/java/strings/android_chrome_strings.grd

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -508,6 +508,13 @@ public class PrefServiceBridge {
     }
 
     /**
+     * @param whether HTTPSE should be enabled.
+     */
+    public void setHTTPSEEnabled(boolean enabled) {
+        PrefServiceBridgeJni.get().setHTTPSEEnabled(enabled);
+    }
+
+    /**
      * @param whether Tracking Protection should be enabled.
      */
     public void setTrackingProtectionEnabled(boolean enabled) {
@@ -1280,5 +1287,6 @@ public class PrefServiceBridge {
         void setForceWebContentsDarkModeEnabled(PrefServiceBridge caller, boolean enabled);
         void setTrackingProtectionEnabled(boolean enabled);
         void setAdBlockEnabled(boolean enabled);
+        void setHTTPSEEnabled(boolean enabled);
     }
 }
