bad216da791e58b4adf9d33bfd3092e93d3bb8aa
Serg <serg.zhukovsky@gmail.com>
2017-04-01 19:46:30 -0400

    Sync functionality(not finished)

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -687,7 +687,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
      * {@link Toolbar}. Extending classes can override this call to avoid creating the toolbar.
      */
     protected void initializeToolbar() {
-<<<<<<< HEAD
         try (TraceEvent te = TraceEvent.scoped("ChromeActivity.initializeToolbar")) {
             final View controlContainer = findViewById(R.id.control_container);
             assert controlContainer != null;
@@ -1602,6 +1601,12 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (mDirectActionInitializer != null) {
             registerDirectActions();
         }
+
+        // Starting Brave Sync
+        /*ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app) {
+            app.mBraveSyncWorker = new BraveSyncWorker(this);
+        }*/
     }
 
     /**
