3f1f7f0d8528c1eb79543fc9e54fa9f0eb5fd8af
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-05 14:57:01 -0400

    Add stats frequency

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -41,6 +41,7 @@ import org.chromium.chrome.browser.dependency_injection.DaggerChromeAppComponent
 import org.chromium.chrome.browser.dependency_injection.ModuleFactoryOverrides;
 import org.chromium.chrome.browser.flags.FeatureUtilities;
 import org.chromium.chrome.browser.BraveSyncWorker;
+import org.chromium.chrome.browser.StatsUpdaterWorker;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.chrome.browser.init.ShieldsConfig;
 import org.chromium.chrome.browser.locale.LocaleManager;
@@ -64,6 +65,8 @@ public class ChromeApplication extends Application {
     private ShieldsConfig mShieldsConfig;
     // Sync worker
     public BraveSyncWorker mBraveSyncWorker = null;
+    // Stats updater
+    public StatsUpdaterWorker mStatsUpdaterWorker = null;
 
     /** Lock on creation of sComponent. */
     private static final Object sLock = new Object();
