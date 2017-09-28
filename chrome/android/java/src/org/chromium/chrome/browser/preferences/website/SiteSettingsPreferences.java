78f1349b0fc3fb5a32acae3f0926c63302145a76
AlexeyBarabash <alexey.barabash@gmail.com>
2017-09-28 14:58:19 +0300

    Fix for DesktopMode after QA tests

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
@@ -36,7 +36,6 @@ public class SiteSettingsPreferences
     // are defined in the SiteSettingsCategory, additional keys
     // are listed here.
     static final String MEDIA_KEY = "media";
-    //static final String DESKTOP_VIEW_SETTINGS_KEY = "desktop_view";
 
     // Whether this class is handling showing the Media sub-menu (and not the main menu).
     boolean mMediaSubMenu;
@@ -78,7 +77,6 @@ public class SiteSettingsPreferences
                 getPreferenceScreen().removePreference(findPreference(i));
             }
             getPreferenceScreen().removePreference(findPreference(MEDIA_KEY));
-            //getPreferenceScreen().removePreference(findPreference(DESKTOP_VIEW_SETTINGS_KEY));
         } else {
             // These will be tucked under the Media subkey, so don't show them on the main menu.
             getPreferenceScreen().removePreference(findPreference(Type.AUTOPLAY));
@@ -131,7 +129,6 @@ public class SiteSettingsPreferences
             websitePrefs.add(Type.SENSORS);
             websitePrefs.add(Type.SOUND);
             websitePrefs.add(Type.USB);
-            websitePrefs.add(Type.DESKTOP_VIEW_SETTINGS);
         }
 
         // Initialize the summary and icon for all preferences that have an
@@ -177,9 +174,9 @@ public class SiteSettingsPreferences
                 p.setSummary(ContentSettingsResources.getSoundBlockedListSummary());
             } else if (requiresTriStateSetting) {
                 p.setSummary(ContentSettingsResources.getCategorySummary(setting));
-            }/* else if (SiteSettingsCategory.Type.DESKTOP_VIEW_SETTINGS == prefCategory) {
+            } else if (SiteSettingsCategory.Type.DESKTOP_VIEW_SETTINGS == prefCategory) {
                 p.setSummary( checked ? ContentSettingsResources.getDesktopViewEnabledSummary() : ContentSettingsResources.getDesktopViewDisabledSummary());
-            }*/ else {
+            } else {
                 p.setSummary(ContentSettingsResources.getCategorySummary(contentType, checked));
             }
 
