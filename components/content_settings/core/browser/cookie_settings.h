7be0926a4f90dcbb71b631bf4806f0cdeb3deb18
samartnik <artem@brave.com>
2017-12-07 11:59:38 +0200

    Fixed build after rebase

--- components/content_settings/core/browser/cookie_settings.h
+++ components/content_settings/core/browser/cookie_settings.h
@@ -57,7 +57,7 @@ class CookieSettings : public CookieSettingsBase,
                  PrefService* prefs,
                  bool is_incognito,
                  const char* extension_scheme = kDummyExtensionScheme,
-                 const bool &incognito);
+                 const bool &incognito = false);
 
   // Returns the default content setting (CONTENT_SETTING_ALLOW,
   // CONTENT_SETTING_BLOCK, or CONTENT_SETTING_SESSION_ONLY) for cookies. If
