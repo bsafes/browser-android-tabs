f5de638d736b4dd0d4b4e8ecdaf9c7629c330163
samartnik <artem@brave.com>
2018-01-31 09:35:37 +0200

    Disabled sending metrics

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -104,10 +104,10 @@ public class PrivacyPreferences
         adBlockRegionalPref.setOnPreferenceChangeListener(this);
         adBlockRegionalPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
-        ChromeBaseCheckBoxPreference sendMetricsPref =
+        /*ChromeBaseCheckBoxPreference sendMetricsPref =
                 (ChromeBaseCheckBoxPreference) findPreference(PREF_SEND_METRICS);
         sendMetricsPref.setOnPreferenceChangeListener(this);
-        sendMetricsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
+        sendMetricsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);*/
 
         updateSummaries();
     }
