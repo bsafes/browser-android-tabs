abc744f5dad4e30b7b9e393403efe46d521e6443
samartnik <artem@brave.com>
2018-02-28 19:58:40 +0200

    Sync flow (beta version)

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -247,6 +247,12 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         }
 
         if (item.getItemId() == android.R.id.home) {
+            if (getFragmentForTest() instanceof BraveSyncScreensPreference) {
+                BraveSyncScreensPreference pref = (BraveSyncScreensPreference) getFragmentForTest();
+                if (pref.onBackPressed()) {
+                    return true;
+                }
+            }
             finish();
             return true;
         } else if (item.getItemId() == R.id.menu_id_general_help) {
