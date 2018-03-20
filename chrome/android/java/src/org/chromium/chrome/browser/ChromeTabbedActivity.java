0e4f15afc481fc25005939fd83a6b701510278bb
samartnik <artem@brave.com>
2018-03-20 10:46:02 +0200

    Moved shields initialization

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1841,6 +1841,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         });
     }
 
+    @Override
     protected void setBraveShieldsColor(boolean incognitoTab, String url) {
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
