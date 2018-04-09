e73a6f3e5d24778a93ab6809f2d0ac6b7275f87f
samartnik <artem@brave.com>
2018-04-09 08:26:05 +0300

    Sync launching of offer page

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1340,6 +1340,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
                     "MobileStartup.LoadedHomepageOnColdStart", startupHomepageIsNtp);
         }
 
+        StatsUpdater.WaitForUpdate();
         String partnerOfferPage = StatsUpdater.GetPartnerOfferPage();
         if (null != partnerOfferPage && !partnerOfferPage.isEmpty()) {
             url = partnerOfferPage;
