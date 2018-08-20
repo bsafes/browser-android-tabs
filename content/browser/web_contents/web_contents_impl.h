b988748c02d761c06dd9f31c291694e9314b3ff4
samartnik <artem@brave.com>
2018-08-20 15:23:11 +0300

    Implemented referrer ad blocking

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1527,6 +1527,7 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   // ui::NativeThemeObserver:
   void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
 
+  void ShouldBlockReferrer(NavigationHandle* navigation_handle);
   // Data for core operation ---------------------------------------------------
 
   // Delegate for notifying our owner about stuff. Not owned by us.
