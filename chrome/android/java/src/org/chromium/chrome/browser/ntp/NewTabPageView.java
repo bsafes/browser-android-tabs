424dd1f89de411e0ac952f0ac1d2e162c7f28ad7
samartnik <artem@brave.com>
2018-09-13 09:32:08 +0300

    Fixed NTP

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -210,7 +210,7 @@ public class NewTabPageView extends HistoryNavigationLayout {
         initializeLayoutChangeListener();
         mNewTabPageLayout.setSearchProviderInfo(searchProviderHasLogo, searchProviderIsGoogle);
 
-        //mRecyclerView.init(mUiConfig, closeContextMenuCallback);
+        mRecyclerView.init(mUiConfig, closeContextMenuCallback);
 
         // Set up snippets
         NewTabPageAdapter newTabPageAdapter = new NewTabPageAdapter(
@@ -274,7 +274,7 @@ public class NewTabPageView extends HistoryNavigationLayout {
      *                        has focus.
      */
     public void setFakeboxDelegate(FakeboxDelegate fakeboxDelegate) {
-        //mRecyclerView.setFakeboxDelegate(fakeboxDelegate);
+        mRecyclerView.setFakeboxDelegate(fakeboxDelegate);
     }
 
     private void initializeLayoutChangeListener() {
