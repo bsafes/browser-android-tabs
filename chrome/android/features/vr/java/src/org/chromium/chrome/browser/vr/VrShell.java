752d52a769a6cc3c7cc0691058c66f0f77a7d1d0
samartnik <artem@brave.com>
2018-10-19 16:08:15 +0300

    Fixed build for cr-70

--- chrome/android/features/vr/java/src/org/chromium/chrome/browser/vr/VrShell.java
+++ chrome/android/features/vr/java/src/org/chromium/chrome/browser/vr/VrShell.java
@@ -1115,7 +1115,7 @@ public class VrShell extends GvrLayout
 
     @CalledByNative
     public void openFeedback() {
-        mActivity.onMenuOrKeyboardAction(R.id.help_id, true);
+        //mActivity.onMenuOrKeyboardAction(R.id.help_id, true);
     }
 
     private void updateHistoryButtonsVisibility() {
