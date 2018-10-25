1b77d515496d018a33cf8df6a6035e86a9e5eb28
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-10-25 11:59:37 -0400

    added Sam's popup window for rewards-panel

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2424,6 +2424,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             DomDistillerUIUtils.openSettings(currentTab.getWebContents());
         } else if (id == R.id.brave_set_default_browser) {
           handleBraveSetDefaultBrowserDialog();
+        } else if (id == R.id.brave_rewards_id) {
+            showBraveRewardsPanel();
         } else if (id == R.id.exit_id) {
             ApplicationLifetime.terminate(false);
         } else {
@@ -2496,6 +2498,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 return;
             }
         }
+    }
+
+    private void showBraveRewardsPanel() {
+        PopupActivity.show(this);
     }
 
     /**
