5d51aa1c6c036e4d317c7512b49daf89288c0618
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-10-26 14:36:33 -0400

    Fixes after cr70 rebase

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -385,8 +385,8 @@ public class FeatureUtilities {
      */
     public static boolean isBottomToolbarEnabled() {
         // TODO(crbug.com/944228): TabGroupsAndroid and ChromeDuet are incompatible for now.
-        return isFlagEnabled(BOTTOM_TOOLBAR_ENABLED_KEY, false)
-                && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(
+        return /*isFlagEnabled(BOTTOM_TOOLBAR_ENABLED_KEY, false)
+                && */!DeviceFormFactor.isNonMultiDisplayContextOnTablet(
                         ContextUtils.getApplicationContext())
                 && !isTabGroupsAndroidEnabled();
     }
