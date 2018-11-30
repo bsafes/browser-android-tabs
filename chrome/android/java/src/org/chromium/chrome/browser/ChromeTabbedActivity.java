41864a78f37bcd22e421135f2365a0f708096b45
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-30 16:38:14 -0500

    JNI link to RewardsService

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -51,6 +51,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveRewardsNativeWorker;
 import org.chromium.chrome.browser.init.StatsUpdater;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
@@ -312,6 +313,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
     private OverviewModeBehavior.OverviewModeObserver mOverviewModeObserver;
 
     private boolean mPartnerPageIsLoaded;
+    private BraveRewardsNativeWorker mBraveRewardsNativeWorker;
 
     private final IncognitoTabHost mIncognitoTabHost = new IncognitoTabHost() {
 
@@ -494,6 +496,11 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         } else {
             mMultiInstanceManager = null;
         }
+    }
+
+    public BraveRewardsNativeWorker getBraveRewardsNativeWorker() {
+        assert mBraveRewardsNativeWorker != null;
+        return mBraveRewardsNativeWorker;
     }
 
     @Override
@@ -1652,6 +1659,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
 
         mUndoBarPopupController =
                 new UndoBarController(this, mTabModelSelectorImpl, this::getSnackbarManager);
+        mBraveRewardsNativeWorker = new BraveRewardsNativeWorker();
     }
 
     @Override
@@ -2248,6 +2256,11 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         }
 
         IncognitoTabHostRegistry.getInstance().unregister(mIncognitoTabHost);
+
+        if (mBraveRewardsNativeWorker != null) {
+            mBraveRewardsNativeWorker.Destroy();
+            mBraveRewardsNativeWorker = null;
+        }
         super.onDestroyInternal();
     }
 
