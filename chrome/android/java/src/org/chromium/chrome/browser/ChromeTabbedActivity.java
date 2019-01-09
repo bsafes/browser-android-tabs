948741cec988b5d17d4bee33b303b04f20453044
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-01-09 11:21:55 -0500

    made BraveRewardsNativeWorker singleton

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -49,7 +49,6 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.BraveRewardsNativeWorker;
 import org.chromium.chrome.browser.init.StatsUpdater;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
@@ -540,11 +539,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         } else {
             mMultiInstanceManager = null;
         }
-    }
-
-    public BraveRewardsNativeWorker getBraveRewardsNativeWorker() {
-        assert mBraveRewardsNativeWorker != null;
-        return mBraveRewardsNativeWorker;
     }
 
     @Override
@@ -1739,7 +1733,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
 
         mUndoBarPopupController =
                 new UndoBarController(this, mTabModelSelectorImpl, this::getSnackbarManager);
-        mBraveRewardsNativeWorker = new BraveRewardsNativeWorker();
     }
 
     @Override
@@ -2330,11 +2323,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
         }
 
         IncognitoTabHostRegistry.getInstance().unregister(mIncognitoTabHost);
-
-        if (mBraveRewardsNativeWorker != null) {
-            mBraveRewardsNativeWorker.Destroy();
-            mBraveRewardsNativeWorker = null;
-        }
         super.onDestroyInternal();
     }
 
