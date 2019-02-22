9ad551005353e66f0860efe2ddc9e41c44a57712
Albert Wang <amwang217@gmail.com>
2019-02-22 09:05:32 -0800

    Fix toolbar for home button, new tab button, and don't change colors for top toolbar

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -220,7 +220,7 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
     private boolean mInitializedWithNative;
     private Runnable mOnInitializedRunnable;
 
-    private boolean mShouldUpdateToolbarPrimaryColor = true;
+    private final boolean mShouldUpdateToolbarPrimaryColor = false;
     private int mCurrentThemeColor;
 
     private OmniboxStartupMetrics mOmniboxStartupMetrics;
@@ -1578,7 +1578,8 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
      *                     from the Tab.
      */
     public void setShouldUpdateToolbarPrimaryColor(boolean shouldUpdate) {
-        mShouldUpdateToolbarPrimaryColor = shouldUpdate;
+        // (Albert Wang): Disabled since we have locked toolbar colors
+        // mShouldUpdateToolbarPrimaryColor = shouldUpdate;
     }
 
     /**
