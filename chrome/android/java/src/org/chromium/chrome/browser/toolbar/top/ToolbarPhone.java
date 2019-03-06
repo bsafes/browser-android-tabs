0d9a17fe6d2c3da9eb8c22db088fc80b56200f12
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-03-05 23:23:14 -0500

    Hide ac options when ac is off. Fix# 1138

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2113,6 +2113,9 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     @Override
     public void OnGetRewardsMainEnabled(boolean enabled) {}
 
+    @Override
+    public void OnGetAutoContributeProps() {}
+
 
     @Override
     public void setOnTabSwitcherClickHandler(OnClickListener listener) {
