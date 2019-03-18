b7d18f05ff5a1665598076f53cb9277bbde8a28b
samartnik <artem@brave.com>
2019-03-18 11:54:59 -0400

    Fixed build for cr-73

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1066,7 +1066,7 @@ base::FilePath GetModulePath(base::StringPiece16 module_name) {
 
 }  // namespace
 
-std::string GetUserAgent() {
+std::string GetUserAgent(const std::string& host) {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kUserAgent)) {
     std::string ua = command_line->GetSwitchValueASCII(switches::kUserAgent);
@@ -1086,7 +1086,7 @@ std::string GetUserAgent() {
   if (command_line->HasSwitch(switches::kUseMobileUserAgent))
     product += " Mobile";
 #endif
-  return content::BuildUserAgentFromProduct(product);
+  return content::BuildUserAgentFromProduct(product, host);
 }
 
 blink::UserAgentMetadata GetUserAgentMetadata() {
@@ -5638,8 +5638,8 @@ std::string ChromeContentBrowserClient::GetProduct() {
   return ::GetProduct();
 }
 
-std::string ChromeContentBrowserClient::GetUserAgent() {
-  return ::GetUserAgent();
+std::string ChromeContentBrowserClient::GetUserAgent(const std::string& host) {
+  return ::GetUserAgent(host);
 }
 
 blink::UserAgentMetadata ChromeContentBrowserClient::GetUserAgentMetadata() {
