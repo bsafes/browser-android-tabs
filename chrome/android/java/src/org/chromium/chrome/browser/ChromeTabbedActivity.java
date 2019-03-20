b60d551f23f007560f2a6650fe3c578904453b2c
samartnik <artem@brave.com>
2019-03-20 13:34:50 -0400

    Fixed issues after cr-73 rebase

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1907,7 +1907,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
     }
 
     @Override
-    protected void setBraveShieldsColor(boolean incognitoTab, String url) {
+    public void setBraveShieldsColor(boolean incognitoTab, String url) {
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
             if (app.getShieldsConfig().isTopShieldsEnabled(incognitoTab, url)) {
