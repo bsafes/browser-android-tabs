f97f8fe8c5520c9c70e407b2ebd9a6dfec937876
samartnik <artem@brave.com>
2019-03-22 11:27:19 -0400

    Added disable bootom toolbar menu item. Changed icons.

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -181,7 +181,7 @@ public class FeatureUtilities {
         cacheHomePageButtonForceEnabled();
         cacheHomepageTileEnabled();
         cacheNewTabPageButtonEnabled();
-        cacheBottomToolbarEnabled();
+        //cacheBottomToolbarEnabled();
         cacheAdaptiveToolbarEnabled();
         cacheLabeledBottomToolbarEnabled();
         cacheNightModeAvailable();
@@ -451,7 +451,8 @@ public class FeatureUtilities {
      * @return Whether or not the bottom toolbar is enabled.
      */
     public static boolean isBottomToolbarEnabled() {
-        if (sIsBottomToolbarEnabled == null) {
+        return ChromePreferenceManager.getInstance().isBottomToolbarEnabled();
+        /*if (sIsBottomToolbarEnabled == null) {
             ChromePreferenceManager prefManager = ChromePreferenceManager.getInstance();
 
             sIsBottomToolbarEnabled = true;
@@ -460,7 +461,7 @@ public class FeatureUtilities {
         return sIsBottomToolbarEnabled
                 && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(
                         ContextUtils.getApplicationContext())
-                && !isTabGroupsAndroidEnabled();
+                && !isTabGroupsAndroidEnabled();*/
     }
 
     /**
