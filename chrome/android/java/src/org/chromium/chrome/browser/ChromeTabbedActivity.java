cece4ec3f29ebabfb2f81cf7524f824df6c08fdd
samartnik <artem@brave.com>
2019-05-06 14:27:28 -0400

    Fixed build for cr-74

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1871,7 +1871,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements ScreenshotMo
                if (false == tab.isDesktopModeOverridenByTab()) {
                    final boolean desktopViewFromSettings = PrefServiceBridge.getInstance().desktopViewEnabled();
                     //if required UA is already set, do nothing
-                    if (desktopViewFromSettings != tab.getUseDesktopUserAgent() ) {
+                    if (desktopViewFromSettings != tab.getUseDesktopUserAgent()) {
                         tab.setUseDesktopUserAgent(desktopViewFromSettings, false);
                     }
                 }
