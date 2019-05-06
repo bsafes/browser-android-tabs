914364877a46f25f3b8da545604b327373291d5d
samartnik <artem@brave.com>
2019-05-06 14:27:28 -0400

    Fixed build for cr-74

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
@@ -319,7 +319,7 @@ public class MainPreferences extends PreferenceFragmentCompat
 
     @Override
     public void syncStateChanged() {
-        updateSyncAndServicesPreference();
+        //updateSyncAndServicesPreference();
     }
 
     @VisibleForTesting
