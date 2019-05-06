fda634066145ec6cc9cf9d9879bdf415519affa2
samartnik <artem@brave.com>
2019-05-06 14:27:28 -0400

    Fixed build for cr-74

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/Preferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/Preferences.java
@@ -267,6 +267,12 @@ public class Preferences extends ChromeBaseAppCompatActivity
     @Override
     public void onBackPressed() {
         Fragment activeFragment = getMainFragment();
+        if (activeFragment instanceof BraveSyncScreensPreference) {
+            BraveSyncScreensPreference pref = (BraveSyncScreensPreference) getFragmentForTest();
+            if (pref.onBackPressed()) {
+                return;
+            }
+        }
         if (!(activeFragment instanceof OnBackPressedListener)) {
             super.onBackPressed();
             return;
