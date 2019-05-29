f76f20d633b9c7da5590a2afaa82e3f4fe2cf228
samartnik <artem@brave.com>
2019-05-29 14:58:27 -0400

    Fixed build for cr-75

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -417,6 +417,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
         super.destroy();
         if (mHomeButton != null) mHomeButton.destroy();
         cancelAnimations();
+        if (mBraveRewardsNativeWorker != null) mBraveRewardsNativeWorker.RemoveObserver(this);
     }
 
     /**
@@ -2074,13 +2075,6 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
             mDelayedTabSwitcherModeAnimation.start();
         } else {
             updateViewsForTabSwitcherMode();
-        }
-    }
-
-    @Override
-    public void destroy() {
-        if (mBraveRewardsNativeWorker != null) {
-            mBraveRewardsNativeWorker.RemoveObserver(this);
         }
     }
 
