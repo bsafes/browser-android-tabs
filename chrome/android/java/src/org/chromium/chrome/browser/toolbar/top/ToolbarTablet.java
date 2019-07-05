52dd5c822028eeababee9356067fd22e0aaaed08
Terry Mancey <terry.mancey@icloud.com>
2019-07-05 23:07:38 +0100

    Fix enable rewards doesn't auto enable ads

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -434,16 +434,18 @@ public class ToolbarTablet extends ToolbarLayout
 
     private boolean mayShowBraveAdsOobeDialog() {
         Context context = getContext();
+
         if (BraveAdsSignupDialog.shouldShowNewUserDialog(context)) {
+            BraveAdsSignupDialog.showNewUserDialog(getContext());
+            return true;
+        } else if (BraveAdsSignupDialog.shouldShowNewUserDialogIfRewardsIsSwitchedOff(context)) {
             BraveAdsSignupDialog.showNewUserDialog(getContext());
             return true;
         } else if (BraveAdsSignupDialog.shouldShowExistingUserDialog(context)) {
             BraveAdsSignupDialog.showExistingUserDialog(getContext());
             return true;
-        } else if (BraveAdsSignupDialog.shouldShowForUserWhoNeverTurnedOnRewards(context)) {
-            BraveAdsSignupDialog.showNewUserDialog(getContext());
-            return true;
         }
+
         return false;
     }
 
@@ -619,8 +621,7 @@ public class ToolbarTablet extends ToolbarLayout
         boolean shouldShow =
           PackageUtils.isFirstInstall(getContext())
           && !shownBefore
-          && !rewardsEnabled
-          && !BraveRewardsPanelPopup.wasBraveRewardsExplicitlyTurnedOff();
+          && !rewardsEnabled;
 
         if (!shouldShow) return;
 
