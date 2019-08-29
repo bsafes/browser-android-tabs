d980fbd6533cc4df3356274aa21b64744c89929b
samartnik <artem@brave.com>
2019-08-29 14:05:28 -0400

    Fixed adblock on Lollipop and lower devices

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -222,6 +222,10 @@ import java.util.Set;
 import java.util.function.Consumer;
 import android.content.pm.PackageManager;
 
+import com.google.android.gms.security.ProviderInstaller;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+
 /**
  * A {@link AsyncInitializationActivity} that builds and manages a {@link CompositorViewHolder}
  * and associated classes.
@@ -233,6 +237,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                    ContextualSearchTabPromotionDelegate, SnackbarManageable, SceneChangeObserver,
                    StatusBarColorController.StatusBarColorProvider, AppMenuDelegate, AppMenuBlocker,
                    MenuOrKeyboardActionController {
+
+    private static final String TAG = "ChromeActivity";
 
     /**
      * No control container to inflate during initialization.
@@ -417,6 +423,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         }
 
         getWindow().setBackgroundDrawable(getBackgroundDrawable());
+
+        updateAndroidSecurityProvider(this);
     }
 
     protected RootUiCoordinator createRootUiCoordinator() {
@@ -2908,5 +2916,20 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     @VisibleForTesting
     public RootUiCoordinator getRootUiCoordinatorForTesting() {
         return mRootUiCoordinator;
+    }
+
+    private void updateAndroidSecurityProvider(Context context) {
+        // We need it on Lollipop and lower
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                ProviderInstaller.installIfNeeded(context);
+            } catch (GooglePlayServicesRepairableException e) {
+                Log.e(TAG, "GooglePlayServicesRepairableException: " + e);
+                assert(false);
+            } catch (GooglePlayServicesNotAvailableException e) {
+                Log.e(TAG, "GooglePlayServicesNotAvailableException: " + e);
+                assert(false);
+            }
+        }
     }
 }
