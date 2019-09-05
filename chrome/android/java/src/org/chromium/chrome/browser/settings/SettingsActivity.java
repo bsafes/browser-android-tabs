0e3ec0bf974eee14c5523fb3bd40f4035d8910bb
samartnik <artem@brave.com>
2019-09-05 12:14:16 -0400

    Fixes settings for cr-77

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -247,6 +247,12 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
         }
 
         if (item.getItemId() == android.R.id.home) {
+            if (mainFragmentCompat instanceof OnBackPressedListener) {
+                OnBackPressedListener listener = (OnBackPressedListener) mainFragmentCompat;
+                if (listener.onBackPressed()) {
+                    return true;
+                }
+            }
             finish();
             return true;
         } else if (item.getItemId() == R.id.menu_id_general_help) {
