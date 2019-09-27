97566f2d9f9e287e3dc4cb0c8fba438dab57e494
samartnik <artem@brave.com>
2019-09-27 11:03:56 -0400

    Fixed build for cr-77

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/themes/RadioButtonGroupThemePreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/themes/RadioButtonGroupThemePreference.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
+import android.view.View;
 
 import org.chromium.base.BuildInfo;
 import org.chromium.base.VisibleForTesting;
