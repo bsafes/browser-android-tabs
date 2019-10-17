remzert/BraveBrowser

AdBlock, tracking protection, h264, aac support
Auditors: @bbondy

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -351,6 +351,18 @@ static void SetSafeBrowsingEnabled(JNIEnv* env,
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void SetAdBlockEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
+}
+
+static void SetTrackingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kTrackingProtectionEnabled, enabled);
+}
+
 static jboolean GetSafeBrowsingManaged(JNIEnv* env,
                                        const JavaParamRef<jobject>& obj) {
   return GetPrefService()->IsManagedPreference(prefs::kSafeBrowsingEnabled);

bsafes/browser-android-tabs

Conflicts:
	chrome/android/BUILD.gn
	out/Default/obj/chrome/android/chrome_public_apk_assets__build_config.ninja
	out/Default/obj/chrome/android/chrome_public_apk_manifest.ninja
	out/Default/obj/chrome/android/chrome_public_apk_resources__build_config.ninja
	out/Default/obj/chrome/android/chrome_public_apk_resources__process_resources.ninja
	out/Default/obj/chrome/android/chrome_public_apk_template_resources__template.ninja
	out/Default/obj/chrome/android/chrome_public_test_apk_manifest.ninja
	out/Default/obj/chrome/browser/browser.ninja
	chrome/android/chrome_apk.gyp
	chrome/browser/net/chrome_network_delegate.cc
	chrome/chrome_android_paks.gypi
	chrome/chrome_browser.gypi
	media/base/key_systems.cc
	chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
	chrome/android/java/strings/android_chrome_strings.grd
	chrome/common/pref_names.cc
	chrome/common/pref_names.h

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -460,6 +460,18 @@ static void JNI_PrefServiceBridge_SetSafeBrowsingEnabled(
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void JNI_PrefServiceBridge_SetAdBlockEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
+}
+
+static void JNI_PrefServiceBridge_SetTrackingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kTrackingProtectionEnabled, enabled);
+}
+
 static jboolean JNI_PrefServiceBridge_GetSafeBrowsingManaged(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj) {