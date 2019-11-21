remzert/BraveBrowser

Added Canvas and WebGL fingerprinting protection
Auditors: @bbondy

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -342,6 +342,12 @@ static void SetSafeBrowsingEnabled(JNIEnv* env,
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void SetFingerprintingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+  GetPrefService()->SetBoolean(prefs::kFingerprintingProtectionEnabled, enabled);
+}
+
 static void SetHTTPSEEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {

bsafes/browser-android-tabs

Conflicts:
	chrome/common/render_messages.h
	components/content_settings/content/common/content_settings_messages.h
	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
	third_party/WebKit/public/web/WebContentSettingsClient.h
	chrome/browser/renderer_host/chrome_render_message_filter.cc
	chrome/browser/renderer_host/chrome_render_message_filter.h
	chrome/browser/content_settings/tab_specific_content_settings.cc
	chrome/renderer/content_settings_observer.h

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -468,6 +468,12 @@ static void JNI_PrefServiceBridge_SetSafeBrowsingEnabled(
   GetPrefService()->SetBoolean(prefs::kSafeBrowsingEnabled, enabled);
 }
 
+static void JNI_PrefServiceBridge_SetFingerprintingProtectionEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+  GetPrefService()->SetBoolean(prefs::kFingerprintingProtectionEnabled, enabled);
+}
+
 static void JNI_PrefServiceBridge_SetHTTPSEEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {